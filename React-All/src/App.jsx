5fbd05593c5a70c2ac8a1a869f4f9273fec9fcdf
Vivek <vivek@gmail.com>
2026-09-14 20:54:34 +0530

    context updated

--- React-All/src/App.jsx
+++ React-All/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Context from './Components/ContextAPI/Context'
 
 export default function App() {
   return (
     <div>
       <h1>All React Concept</h1>
+      <Context/>
     </div>
   )
 }
---
+++ React-All/src/Components/ContextAPI/Context.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Context() {
+  return (
+    <div>
+      <h1>context API</h1>
+    </div>
+  )
+}
